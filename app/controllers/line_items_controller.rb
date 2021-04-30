c81231ffe7bd837ccc5e429b5810f09240dc0644
Denys Vinegod <denys.vinegod@gmail.com>
2021-04-30 14:35:51 +0300

    Iteration F4, Ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,6 +32,7 @@ class LineItemsController < ApplicationController
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.js   { @current_item = @line_item }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
