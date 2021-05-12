55f213e0f790d9fc70cbd159727ddd019e93a73e
vnegod <denys.vinegod@gmail.com>
2021-05-12 13:45:50 +0300

    I1, mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Mail
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'denys.vinegod',
+    authentication: 'plain',
+    user_name: 'depot@example.com',
+    password: '#F@nta$228',
+    enable_starttls_auto: true
+  }
+end
